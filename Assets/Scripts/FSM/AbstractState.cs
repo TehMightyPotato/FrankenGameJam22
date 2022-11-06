3fbdf6374f7e5d4e61d8a1a45210c99d9a45e4e8
Nicolas Teichtweier <contact@mightypotato.de>
2022-11-06 16:00:56 +0100

    wip

---
+++ Assets/Scripts/FSM/AbstractState.cs
@@ -0,0 +1,9 @@
+﻿namespace FSM
+{
+    public abstract class AbstractState
+    {
+        public abstract void StateEnter();
+
+        public abstract void StateExit();
+    }
+}
---
+++ Assets/Scripts/UI/RearMirrorHandler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using MyBox;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UI
+{
+    public class RearMirrorHandler : MonoBehaviour
+    {
+        [Separator("Default State")] public GameObject defaultStateObj;
+        
+        public UnityEvent<RearMirrorState> OnStateChanged;
+
+        [SerializeField] private RearMirrorState currentState;
+
+        private Coroutine _stateChangeRoutine;
+        
+        public void ChangeState(RearMirrorState state)
+        {
+            if (_stateChangeRoutine != null) return;
+            switch (state)
+            {
+                case RearMirrorState.Default:
+                    _stateChangeRoutine = StartCoroutine(DefaultStateRoutine());
+                    break;
+                case RearMirrorState.Happy:
+                    _stateChangeRoutine = StartCoroutine(HappyStateRoutine());
+                    break;
+                case RearMirrorState.Sad:
+                    _stateChangeRoutine = StartCoroutine(SadStateRoutine());
+                    break;
+                case RearMirrorState.Drinking:
+                    _stateChangeRoutine = StartCoroutine(DrinkingStateRoutine());
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(state), state, null);
+            }
+
+            currentState = state;
+            OnStateChanged?.Invoke(state);
+        }
+
+        private void StateCleanup()
+        {
+            _stateChangeRoutine = null;
+        }
+
+        private IEnumerator DefaultStateRoutine()
+        {
+            
+        }
+
+        private IEnumerator HappyStateRoutine()
+        {
+            
+        }
+
+        private IEnumerator SadStateRoutine()
+        {
+            
+        }
+
+        private IEnumerator DrinkingStateRoutine()
+        {
+            
+        }
+    }
+
+    public enum RearMirrorState
+    {
+        Default,
+        Happy,
+        Sad,
+        Drinking
+    }
+}
