f38e2488b4e0b251bba3b02dcf5a4b8f256d8221
Vincent <agpheaddikilla@yahoo.de>
2022-11-06 02:10:18 +0100

    feat: add sound ;)

---
+++ Assets/Scripts/PlanetHitHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Planets;
+using Planets.Needs;
+using Shooting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public sealed class ProjectileEnteredEventArgs
+{
+    public Planet Planet { get; }
+    public Projectile Projectile { get; }
+
+    public ProjectileEnteredEventArgs(Planet planet, Projectile projectile)
+    {
+        Planet = planet;
+        Projectile = projectile;
+    }
+}
+
+[CreateAssetMenu(fileName = "PlanetHitHandler", menuName = "PlanetHitHandler")]
+public class PlanetHitHandler : ScriptableObject
+{
+    public UnityEvent<ProjectileEnteredEventArgs> OnProjectileEntered;
+
+    public void Init()
+    {
+        OnProjectileEntered.RemoveAllListeners();
+    }
+
+    public void ProjectileEntered(Planet planet, Projectile projectile)
+    {
+        OnProjectileEntered?.Invoke(new ProjectileEnteredEventArgs(planet, projectile));
+    }
+}
